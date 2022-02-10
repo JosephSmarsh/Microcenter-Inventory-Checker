935dfcf5924502fc29b8e1fbf5b7dae08049327b
JosephSmarsh <smarshjoe@yahoo.com>
2022-02-10 15:55:20 -0700

    Created setup.py to edit config.txt and validate config.txt

---
+++ setup.py
@@ -0,0 +1,174 @@
+# Used to validate productlink
+import requests
+
+# Contains all config values
+class Config:
+    def __init__(self, refreshtime, savecsv, sendnotification, pushsaferkey, deviceid, productlink):
+        self.refreshtime = refreshtime
+        self.savecsv = savecsv
+        self.sendnotification = sendnotification
+        self.pushsaferkey = pushsaferkey
+        self.deviceid = deviceid
+        self.productlink = productlink
+
+    # Check link contains base microcenter link, and link works
+    def isvalidlink(self):
+        if 'https://www.microcenter.com/product/' in self.productlink:
+            if requests.get(self.productlink).status_code == 200:
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    # Check to validate if a y or n answer only contains y or n
+    def isvalidsavecsv(self):
+        if self.savecsv.lower() == 'y' or self.savecsv.lower() == 'n':
+            return True
+        else:
+            return False
+
+    # Check sendnotification is only y or n
+    def isvalidsendnotification(self):
+        if self.sendnotification.lower() == 'y' or self.sendnotification.lower() == 'n':
+            return True
+        else:
+            return False
+
+    # If send noticiation is y, ensure pushsaferkey and deviceid have values
+    def isvalidpushsafer(self):
+        if self.sendnotification.lower() == 'y':
+            if self.pushsaferkey == '' or self.deviceid == '':
+                return False
+            else:
+                return True
+        else:
+            return True
+
+    # Check refresh time is an int greater than 59
+    def isvalidrefresh(self):
+        try:
+            if int(self.refreshtime) > 59:
+                return True
+            else:
+                return False
+        except ValueError as e:
+            return False
+
+    # Set initial csv file, Writes over any existing file
+    def setcsv(self):
+        if self.savecsv.lower() == 'y':
+            with open('historicaldata.csv', 'w') as f:
+                f.write('ID, INVENTORY, TIME, DATE')
+                f.close()
+
+    # Open config.txt, print the contents line by line
+    def printconfig(self):
+        print('________________________________________________________________________')
+        print('                         Current config                                 ')
+        print('________________________________________________________________________')
+        with open('config.txt', 'r') as f:
+            lines = f.readlines()
+            for i in range(len(lines)):
+                print(lines[i].strip())
+            f.close()
+        print('________________________________________________________________________')
+
+    # Validate each item in config, print valid or invalid, then validate entire config and print valid or invalid
+    def printvalidation(self):
+        if self.isvalidrefresh() is True:
+            print('refreshtime          VALID')
+        else:
+            print('refreshtime          INVALID')
+
+        if self.isvalidsavecsv() is True:
+            print('savecsv              VALID')
+        else:
+            print('savecsv              INVALID')
+
+        if self.isvalidsendnotification() is True:
+            print('sendnotification     VALID')
+        else:
+            print('sendnotification     INVALID')
+
+        if self.isvalidpushsafer() is True:
+            print('pushsaferkey         VALID')
+            print('deviceid             VALID')
+        else:
+            print('pushsaferkey         INVALID')
+            print('deviceid             INVALID')
+
+        if self.isvalidlink() is True:
+            print('microcenter          VALID')
+        else:
+            print('microcenter          INVALID')
+
+        print('________________________________________________________________________')
+
+        if self.isvalidconfig() is True:
+            print('Current config       VALID')
+        # If config is invalid, print invalid
+        else:
+            print('Current config       INVALID')
+
+        print('________________________________________________________________________')
+
+    # If all validations are true, config is valid (True), else config is invalid (False)
+    def isvalidconfig(self):
+        if self.isvalidrefresh() and self.isvalidlink() and \
+                self.isvalidsavecsv() and self.isvalidpushsafer() and \
+                self.isvalidsendnotification() is True:
+            return True
+        else:
+            return False
+
+    # Read current config.txt file, ask for input for value, write file line by line
+    def buildconfig(self):
+        # Open config and add lines to lines
+        with open('config.txt', 'r') as f:
+            lines = f.readlines()
+            f.close()
+        # Ask user for input, edit line to include new input
+        for i in range(len(lines)):
+            lines[i] = lines[i].strip().split('=')[0] + '= ' + input('Input ' + str(lines[i].split(' ')[0]) + ': ')
+        # Write new lines variable to config.txt
+        with open('config.txt', 'w') as f:
+            for i in range(len(lines)):
+                f.write(lines[i].strip() + '\n')
+            f.close()
+
+
+# Creates a config object based on config.txt
+def createconfigobj():
+    with open('config.txt', 'r') as f:
+        variables = [line.strip() for line in f]
+        f.close()
+    refreshtime = variables[0].split('=')[1].replace(' ', '')
+    savecsv = variables[1].split('=')[1].replace(' ', '')
+    sendnotification = variables[2].split('=')[1].replace(' ', '')
+    pushsaferkey = variables[3].split('=')[1].replace(' ', '')
+    deviceid = variables[4].split('=')[1].replace(' ', '')
+    productlink = variables[5].split('=')[1].replace(' ', '')
+
+    config = Config(refreshtime, savecsv, sendnotification, pushsaferkey, deviceid, productlink)
+    return config
+
+
+def main():
+    # Initialize config object: happens everytime main is started
+    config = createconfigobj()
+    # Print current config
+    config.printconfig()
+    # Print current validation
+    config.printvalidation()
+    # If user wants to change config, run buildconfig, then loop main again
+    if input('Edit config (y/n): ').lower() == 'y':
+        config.buildconfig()
+        main()
+    # Else escape script
+    else:
+        print('Closing config editor...')
+
+
+if __name__ == "__main__":
+    main()
